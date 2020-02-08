415502be1fd28097cac6c1c0bd0ae9c4283e815b
Colin McFarlane <colinmcf0@gmail.com>
2020-02-08 17:17:55 -0500

    Initial app created. Basic UI created.

---
+++ app/src/main/java/ca/uottawa/cmcfa039/liftcare/HomeActivity.java
@@ -0,0 +1,63 @@
+package ca.uottawa.cmcfa039.liftcare;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.drawerlayout.widget.DrawerLayout;
+
+
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+
+import com.google.android.material.navigation.NavigationView;
+
+public class HomeActivity extends AppCompatActivity {
+
+    private DrawerLayout drawerLayout;
+    private ActionBarDrawerToggle actionBarDrawerToggle;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        drawerLayout = findViewById(R.id.drawer_layout);
+        actionBarDrawerToggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        actionBarDrawerToggle.setDrawerIndicatorEnabled(true);
+        drawerLayout.addDrawerListener(actionBarDrawerToggle);
+        actionBarDrawerToggle.syncState();
+
+        NavigationView nav_view = findViewById(R.id.navigationView);
+
+        getSupportActionBar().setHomeButtonEnabled(false);
+
+        nav_view.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                int id = menuItem.getItemId();
+
+                if (id == R.id.nav_helicopter) {
+                    Toast.makeText(HomeActivity.this, "HELICOPTER", Toast.LENGTH_LONG);
+                }
+
+                else if (id == R.id.nav_hospital){
+                    Toast.makeText(HomeActivity.this, "HOSPITAL", Toast.LENGTH_LONG);
+                }
+
+                return true;
+            }
+        });
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        return actionBarDrawerToggle.onOptionsItemSelected(item) || super.onOptionsItemSelected(item);
+    }
+}
--- app/src/main/java/ca/uottawa/cmcfa039/liftcare/MainActivity.java
+++ app/src/main/java/ca/uottawa/cmcfa039/liftcare/MainActivity.java
@@ -2,7 +2,12 @@ package ca.uottawa.cmcfa039.liftcare;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.os.Bundle;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,6 +15,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button b = findViewById(R.id.button);
+
+
+        b.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this,HomeActivity.class));
+            }
+        });
     }
 
 
